a7c0741d486da65d0a34d58cd524b32d583f3ff5
Hack Reactor Student <student@hackreactor.com>
2017-04-07 18:01:39 -0700

    complete send message feature

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,69 @@
-// YOUR CODE HERE:
+var app = {
+  server: 'http://parse.sfm6.hackreactor.com/',
+
+  init: () => {
+  	$(document).ready(function() {
+	  app.fetch();
+		
+	  $('.sendMessage').on('click', function(event) {
+	  	event.preventDefault();
+	    var message = $('input[name=message]').val();
+	    //debugger;
+	    app.renderMessage(message);
+	    //debugger;
+		console.log(message);
+		//debugger;
+	  })
+    });
+  },
+
+  send: message => {
+	$.ajax({
+	  url: 'http://parse.sfm6.hackreactor.com/',
+	  type: 'POST',
+	  data: JSON.stringify(message),
+	  contentType: 'application/json',
+	  success: function (data) {
+	    console.log('chatterbox: Message sent');
+	  },
+	  error: function (data) {
+	    console.error('chatterbox: Failed to send message', data);
+	  }
+	});
+  },
+
+  fetch: () => {
+	$.ajax({
+	  url: 'http://parse.sfm6.hackreactor.com/',
+	  type: 'GET',
+	  contentType: 'application/json',
+	  success: function (data) {
+	    console.log('chatterbox: Message received');
+	  },
+	  error: function (data) {
+	    console.error('chatterbox: Failed to load message', data);
+	  }
+	});  	
+  },
+
+  clearMessages: () => {
+    $("#chats").children().remove();
+  },
+
+  renderMessage: message => {
+  	var $message = $('<div>'+ message +'</div>')
+    $('#chats').append($message);
+    $('form')[0].reset()
+  },
+
+  renderRoom: () => {
+
+  }
+
+
+
+
+};
+
+
+
